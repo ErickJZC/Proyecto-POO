9e111b9bf7a424a1e5a43595b9e91882373d5be6
ErickJZC <erick.rolito@gmail.com>
2025-11-29 20:18:08 -0500

    Se borro algo, agregado otra vez

---
+++ src/ProyectoFinal/ProyectoFinal.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ProyectoFinal;
+
+import GUI.MainFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author alvar
+ */
+public class ProyectoFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
+    
+}
